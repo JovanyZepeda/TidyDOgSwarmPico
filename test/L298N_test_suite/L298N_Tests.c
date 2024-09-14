a22db9709710f29a9f8222eea28f468b46b37bbb
JovanyZ Base Station 2 <JovanyZepeda.Dev@gmail.com>
2024-09-14 06:47:51 +0000

    Initial draft of tests

--- test/L298N_test_suite/L298N_Tests.c
+++ test/L298N_test_suite/L298N_Tests.c
@@ -1,3 +1,30 @@
 #include <unity.h>
+#include <stdio.h>
 #include "app/L298N.h"
 
+void setUp(void){
+    // Initialize hardware or Mock Objects
+}
+
+
+void tearDown(void){
+    // Clean up after each test
+
+}
+
+
+
+void test_sprintf(void){
+    char output[5] = "";
+
+    TEST_ASSERT_EQUAL(3, sprintf(output, "hey"));
+    TEST_ASSERT_EQUAL_STRING("hey", output);
+}
+
+
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_sprintf);
+    return UNITY_END();
+}
